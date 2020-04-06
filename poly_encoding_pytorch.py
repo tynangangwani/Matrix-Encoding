38eb3266bd3dff73a24017cba684779886e0603e
tynangangwani <tgangwan@usc.edu>
2020-04-06 20:32:33 +0530

    encoding

--- poly_encoding_pytorch.py
+++ poly_encoding_pytorch.py
@@ -1,10 +1,13 @@
 import torch
 
-def polynomial_encode_py(matA, matB, totalWorkers, divA, divB): #takes in matrix A and B, C=A^t*B
-    alpha=1
-    beta=divA
+def polynomial_encode_py(matA, matB, totalWorkers, divA, divB, alpha, beta):
+    #takes in matrix A and B, for the computation C=A^t*B, divA and divB
+    # correspond to the number of parts A and B will be divided into and
+    #alpha and beta are the encoding parameters, usually alpha=1 and beta=divA
+
     m=divA
     n=divB
+    
     splitsA = torch.split(matA, int(matA.size()[0]/divA), dim=0)
     splitsB= torch.split(matB, int(matB.size()[0]/divB), dim=0)
 
